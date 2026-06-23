4085435516d1730eacd5ed3e52a2e88607a45087
Mateo Llallire <mateo.llallire@utec.edu.pe>
2026-06-22 19:39:02 -0500

    Add proxy for backend API

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://hackaton-20261-front-587720740455.us-east1.run.app',
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
